dcf75d825e18b71b02c9bb2301e8b5b11320fd5e
mreilly13 <mrreilly2013@gmail.com>
2023-11-02 18:22:09 -0400

    added layer

--- keyboards/kbdfans/kbd75rgb/keymaps/mreilly13/keymap.c
+++ keyboards/kbdfans/kbd75rgb/keymaps/mreilly13/keymap.c
@@ -16,6 +16,21 @@
 
 #include QMK_KEYBOARD_H
 
+#define BASE 0
+#define LAYER 1
+// Define the keycode, `QK_USER` avoids collisions with existing keycodes
+enum keycodes {
+  KC_MODE = QK_USER,
+};
+
+const key_override_t mode_key_override = ko_make_basic(MOD_BIT(KC_RGUI), KC_SLEP, KC_MODE);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &mode_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_75_ansi(
@@ -24,18 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END,
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_PGUP,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_PGDN,
-    KC_LCTL,  KC_DEL,   KC_LALT,                                KC_SPC,             KC_SLEP,            KC_LGUI,  KC_APP,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_DEL,   KC_LALT,                                KC_SPC,             KC_MODE,            KC_RGUI,  KC_APP,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT_75_ansi(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,    _______,
-    _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
-    _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  _______,  _______,  _______,            _______,  _______,
-    _______,  _______,  _______,                                _______,            _______,            _______,  _______,  _______,  _______,  _______
-  ),
-
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+    _______,  KC_LGUI,  _______,                                _______,            _______,            _______,  _______,  _______,  _______,  _______
+  )/*,
    [2] = LAYOUT_75_ansi(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,    _______,
@@ -51,16 +65,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
     _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  _______,  _______,  _______,            _______,  _______,
     _______,  _______,  _______,                                _______,            _______,            _______,  _______,  _______,  _______,  _______
-  )
+  )*/
 };
 
 void process_rgb_matrix_typing_heatmap(uint8_t row, uint8_t col); // for modification of built-in implementation
 //void process_keypress(uint8_t row, uint8_t col); // for from-scratch implementation
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    process_rgb_matrix_typing_heatmap(record->event.key.row, record->event.key.col); // for modification of built-in implementation
-    //process_keypress(record->event.key.row, record->event.key.col); // for from-scratch implementation
+  switch (keycode) {
+    case KC_MODE:
+      // Our logic will happen on presses, nothing is done on releases
+      if (!record->event.pressed) {
+        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+        return false;
+      }
+      layer_move(get_highest_layer(layer_state) == BASE ? LAYER : BASE);
+      return false;
+
+    // Process other keycodes normally
+    default:
+      if (record->event.pressed) {
+        process_rgb_matrix_typing_heatmap(record->event.key.row, record->event.key.col); // for modification of built-in implementation
+        //process_keypress(record->event.key.row, record->event.key.col); // for from-scratch implementation
+      }
+      return true;
   }
-  return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch (get_highest_layer(state)) {
+    case LAYER:
+      rgb_matrix_sethsv(RGB_MATRIX_DEFAULT_HUE, RGB_MATRIX_DEFAULT_SAT, RGB_MATRIX_DEFAULT_VAL);
+      break;
+    default: //  for any other layers, or the default layer
+      rgb_matrix_sethsv((4+255)/2, RGB_MATRIX_DEFAULT_SAT, RGB_MATRIX_MAXIMUM_BRIGHTNESS);
+      break;
+    }
+  return state;
 }
